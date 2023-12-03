55eb7a97eb423dcd104593c5fa555313060f58e9
mrfels1 <58826166+mrfels1@users.noreply.github.com>
2023-12-04 01:05:18 +0300

    finished main page

---
+++ Production/production.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const track = document.querySelector(".carousel-track");
+  const prevBtn = document.querySelector(".carousel-prev");
+  const nextBtn = document.querySelector(".carousel-next");
+  const products = document.querySelectorAll(".product");
+
+  let currentPosition = 0;
+  const slideWidth = products[0].offsetWidth + 20; // Adjust according to your design
+
+  nextBtn.addEventListener("click", function () {
+    if (currentPosition > -(slideWidth * (products.length - 1))) {
+      currentPosition -= slideWidth;
+      track.style.transform = `translateX(${currentPosition}px)`;
+    }
+  });
+
+  prevBtn.addEventListener("click", function () {
+    if (currentPosition !== 0) {
+      currentPosition += slideWidth;
+      track.style.transform = `translateX(${currentPosition}px)`;
+    }
+  });
+});
