fbcee7bb4159d6530db84df92ba4a639869bc75e
mrfels1 <58826166+mrfels1@users.noreply.github.com>
2023-12-11 03:55:56 +0300

    moving some parts

--- Production/production.js
+++ Production/production.js
@@ -1,4 +1,4 @@
 function scrollToSection(sectionId) {
   const section = document.getElementById(sectionId);
-  section.scrollIntoView({ behavior: 'smooth' });
+  section.scrollIntoView({ behavior: "smooth" });
 }
